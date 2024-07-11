ad76b97ca8d4ec9ac5f1891d49caaf9f825136fa
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-07-11 13:22:45 -0300

    componentes-Banner-Container-com-props-children

--- videoteca/src/App.js
+++ videoteca/src/App.js
@@ -1,3 +1,5 @@
+import Banner from "./components/Banner";
+import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
@@ -5,6 +7,11 @@ export default function App() {
   return (
     <>
         <Header />
+        <Banner image="home"/>
+        <Container>
+                  <h1> Hello Word!</h1>
+                  <p>Olá mundo estou aprendendo React JS!</p>
+        </Container>
        
         <Footer />
     </>
